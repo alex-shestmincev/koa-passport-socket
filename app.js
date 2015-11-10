a043778b082b75d9d805b3aec91bb9d450b6d445
alex.shestmitsev <alex.shestmintsev@altexsoft.com>
2015-11-10 12:37:43 +0200

    app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+// A "closer to real-life" app example
+// using 3rd party middleware modules
+// P.S. MWs calls be refactored in many files
+
+// long stack trace (+clarify from co) if needed
+if (process.env.TRACE) {
+  require('./libs/trace');
+}
+
+var koa = require('koa');
+var app = koa();
+
+var config = require('config');
+var mongoose = require('./libs/mongoose');
+
+// keys for in-koa KeyGrip cookie signing (used in session, maybe other modules)
+app.keys = config.keys;
+
+var path = require('path');
+var fs = require('fs');
+var middlewares = fs.readdirSync(path.join(__dirname, 'middlewares')).sort();
+
+middlewares.forEach(function(middleware) {
+  app.use(require('./middlewares/' + middleware));
+});
+
+const routes = require('./routes')(app);
+
+var socket = require('./libs/socket');
+var server = app.listen(3000);
+socket(server);
